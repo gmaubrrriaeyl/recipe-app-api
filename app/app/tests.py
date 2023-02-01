30614005835b7af17891bd51f8aff13590bd851d
Gabe <gabeemurray@gmail.com>
2023-02-01 08:52:16 -0500

    fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,24 +3,24 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """
     Test the calc module
     """
-    
+
     def test_add_numbers(self):
         """
         Test adding numbers together
         """
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """
         Test subtracting numbers together
         """
-        res = calc.substract(10,15)
+        res = calc.substract(10, 15)
         self.assertEqual(res, 5)
